ff8dbd7b9aca35abbb643e634d8be11446f3cc13
Govind Somalal <govindsomalal7@gmail.com>
2021-04-09 18:21:59 +0530

    original image now scaled for performance
    width settings are dynamically computed as well

--- seam_carver.py
+++ seam_carver.py
@@ -4,13 +4,20 @@ import numpy as np
 import seam_carving
 import streamlit as st
 
-src = np.array(Image.open('./img2.jpg'))
+size = (512,512)
+og   = Image.open('./img.jpg')
+og.thumbnail(size, Image.ANTIALIAS)
 
+src  = np.array(og)
+print(src)
+
+st.image(src)
 src_h, src_w, _ = src.shape
-delta = st.slider('adjust the width', -src_w, 0, 5)
+delta = st.slider('adjust the width', -src_w, 0, 0)
 dst = seam_carving.resize(src, (
     src_w + delta,
     src_h
 ), energy_mode='backward', order='width-first', keep_mask=None)
 morphed = Image.fromarray(dst)
-st.image([src,morphed])
+st.write("After:")
+st.image(morphed)
